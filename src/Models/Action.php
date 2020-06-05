249447105cf40759b8e6d6907fd7925d77bab6fc
JohnAragon <j-aragon@hotmail.com>
2020-06-04 19:11:03 -0500

    Eliminando ambigüedades en modelos existentes

--- src/Models/Action.php
+++ src/Models/Action.php
@@ -3,18 +3,7 @@ namespace Bellpi\HubUsers\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Module extends Model
+class Action extends Model
 {
-	   
-
-	protected $connection = 'hub_users';
-
-	public function available_modules(){
-    	return $this->belongsToMany(Module::class,'module_profile')->withTimeStamps();   
-	} 
-
-	public function available_actions(){
-    	return $this->belongsToMany(Action::class,'action_profile')->withTimeStamps();   
-	} 
-    
+	  
 }
